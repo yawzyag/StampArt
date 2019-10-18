8d48995ba06b1b1fb5a2598ee7b478236f9af93f
yawzyag <yesid.yag@gmail.com>
2019-10-18 19:21:33 +0000

    [Feature] adding some server changes

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -4,11 +4,13 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv/config');
 
+const port = process.env.PORT || 5000;
 const app = express();
 
 // midlewares
 app.use(bodyParser.json());
 app.use(cors());
+app.set('trust proxy', true);
 
 // import Routes
 const productsRoute = require('./routes/products');
@@ -30,4 +32,6 @@ app.get('/', (req, res) => {
 });
 
 // server listen on port
-app.listen(5000);
+app.listen(port, () => {
+    console.log('app listening on port: ', port, '...');
+});
