bca36e6de8812483a5a13cc6a4874cbcb21739a3
AndZapata <andzapata514@gmail.com>
2019-10-23 23:43:15 +0000

    [Fix] navigation bar to render link login and home

--- FrontEnd/src/components/navbar.component.js
+++ FrontEnd/src/components/navbar.component.js
@@ -8,22 +8,27 @@ export class Navbar extends Component {
         class="navbar navbar-light"
         style={{ backgroundColor: "#ffffff", borderBottom: "1px solid #ccc" }}
       >
-        <a class="navbar-brand" href="#" style={letters}>
-          <h3>StampArt</h3>
-        </a>
-
+        <Link to="/">
+          <a class="navbar-brand" style={letters}>
+            <h3>StampArt</h3>
+          </a>
+        </Link>
         <ul class="navbar-nav mr-auto">
           <li class="nav-item active">
-            <a class="nav-link" href="#" style={letters}>
-              Home <span class="sr-only">(current)</span>
-            </a>
+            <Link to="/">
+              <a class="nav-link" style={letters}>
+                Home <span class="sr-only">(current)</span>
+              </a>
+            </Link>
           </li>
         </ul>
 
         <div class="float-right">
-          <button class="btn btn-dark" type="submit">
-            Sign in
+          <Link to="/login">
+            <button class="btn btn-dark" type="submit">
+              Log in
           </button>
+          </Link>
         </div>
       </nav>
     );
