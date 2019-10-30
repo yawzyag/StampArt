415071e39cf327744cd33e50f3f75364cf8ba99a
jeniffervp <737@holbertonschool.com>
2019-10-30 00:35:51 +0000

    [Fix] format and profile pic

--- FrontEnd/src/components/profile.component.js
+++ FrontEnd/src/components/profile.component.js
@@ -4,65 +4,61 @@ export class Profile extends Component {
   render() {
     return (
       <div>
-        <h1>Profile</h1>
-        <form>
-          <div className="row">
-            <div className="col-6 mt-5">
-              <div class="image-cropper">
-                <img
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5RJT4D00AjuD5xaHqmulkRZka-TWvtu2izvGlP43jctqKXuwo&s"
-                  alt="avatar"
-                  class="profile-pic"
-                />
-              </div>
-              <div className="d-flex justify-content-center">
-                <button type="button" class="btn btn-outline-danger mt-4">
-                  Change Avatar
-                </button>
-              </div>
+        <h1 className="text-center mt-1">Profile</h1>
+        <div className="row">
+          <div className="col-sm-12 col-md-6 mt-2">
+            <div className="image-cropper shadow bg-white">
+              <img
+                src="https://www.redditstatic.com/avatars/avatar_default_06_008985.png"
+                alt="avatar"
+                className="profile-pic"
+              />
             </div>
-            <div className="col-6 mt-5">
-              <div class="form-row">
-                <div class="form-group">
-                  <label for="inputEmail4">Email</label>
-                  <input
-                    type="email"
-                    class="form-control"
-                    id="inputEmail4"
-                    placeholder="Email"
-                  />
-                </div>
-                <div class="form-group">
-                  <label for="inputPassword4">Password</label>
-                  <input
-                    type="password"
-                    class="form-control"
-                    id="inputPassword4"
-                    placeholder="Password"
-                  />
-                </div>
-                <div class="form-group">
-                  <label for="inputAddress">Address</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="inputAddress"
-                    placeholder="1234 Main St"
-                  />
-                </div>
-                <div class="form-group">
-                  <label for="inputUserName">User Name</label>
-                  <input type="text" class="form-control" id="inputUserName" />
-                </div>
-              </div>
-              <div class="d-flex justify-content-center">
-                <button type="submit" class="btn btn-danger">
-                  Save
-                </button>
-              </div>
+            <div className="d-flex justify-content-center">
+              <button type="button" className="btn btn-outline-danger mt-4">
+                Change Avatar
+              </button>
             </div>
           </div>
-        </form>
+          <div className="formato col-sm-12 col-md-6 mt-2">
+            <div className="form-group">
+              <label htmlFor="inputEmail4">Email</label>
+              <input
+                type="email"
+                className="form-control"
+                id="inputEmail4"
+                placeholder="Email"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="inputPassword4">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="inputPassword4"
+                placeholder="Password"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="inputAddress">Address</label>
+              <input
+                type="text"
+                className="form-control"
+                id="inputAddress"
+                placeholder="1234 Main St"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="inputUserName">User Name</label>
+              <input type="text" className="form-control" id="inputUserName" />
+            </div>
+            <div className="d-flex justify-content-center mb-2">
+              <button type="submit" className="btn btn-danger">
+                Save
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
